3e7e008acc0e272ad8371a5c04808d7309e7e991
yudha <yudhapurbajagad@gmail.com>
2019-10-03 21:22:55 +0700

    Initial Commit

---
+++ Training 6 SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Training 6 SwiftUI
+//
+//  Created by yudha on 03/10/19.
+//  Copyright © 2019 yudha. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
